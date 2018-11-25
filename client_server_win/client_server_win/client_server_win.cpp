f86aa062b002803e2827f674c90bf1d02facc8f7
Ilia <sspsps00@gmail.com>
2018-11-25 20:16:34 +1000

    first realization region growing segmentation

--- client_server_win/client_server_win/client_server_win.cpp
+++ client_server_win/client_server_win/client_server_win.cpp
@@ -6,14 +6,37 @@ using namespace std;
 void PCLClient::handle_connect(const boost::system::error_code& error) {
 	if (error)
 		return;
-	pcl::PointCloud<pcl::PointXYZRGB>::Ptr buf(new pcl::PointCloud<pcl::PointXYZRGB>);
 	while (!viewer_.wasStopped()) {
 		boost::asio::read(socket_, boost::asio::buffer(&nr_points, sizeof(nr_points)));
 		if (nr_points != 0) {
-			buf->points.resize(nr_points);
-			boost::asio::read(socket_, boost::asio::buffer(&buf->points.front(), nr_points * 8 * sizeof(float)));
-			viewer_.showCloud(buf);
+			buf_->points.resize(nr_points);
+			boost::asio::read(socket_, boost::asio::buffer(&buf_->points.front(), nr_points * 8 * sizeof(float)));
+			viewer_.showCloud(getRegSeg());
 		}
 		nr_points = 0;
 	}
 };
+
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr PCLClient::getRegSeg() {
+	pcl::search::Search <pcl::PointXYZRGB>::Ptr tree = boost::shared_ptr<pcl::search::Search<pcl::PointXYZRGB> >(new pcl::search::KdTree<pcl::PointXYZRGB>);
+	/*pcl::IndicesPtr indices(new std::vector <int>);
+	pcl::PassThrough<pcl::PointXYZRGB> pass;
+	pass.setInputCloud(cloud);
+	pass.setFilterFieldName("z");
+	pass.setFilterLimits(0.0, 1.0);
+	pass.filter(*indices);*/
+
+	pcl::RegionGrowingRGB<pcl::PointXYZRGB> reg;
+	reg.setInputCloud(buf_);
+	//reg.setIndices(indices);
+	reg.setSearchMethod(tree);
+	reg.setDistanceThreshold(10);
+	reg.setPointColorThreshold(6);
+	reg.setRegionColorThreshold(5);
+	reg.setMinClusterSize(600);
+
+	std::vector <pcl::PointIndices> clusters;
+	reg.extract(clusters);
+
+	return reg.getColoredCloud();
+}
--- client_server_win/client_server_win/client_server_win.h
+++ client_server_win/client_server_win/client_server_win.h
@@ -10,6 +10,9 @@
 #include <pcl/point_types.h>
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/visualization/cloud_viewer.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/segmentation/region_growing_rgb.h>
+#include <pcl/search/kdtree.h>
 
 #include <boost/system/config.hpp>
 #include <boost/bind.hpp>
@@ -25,13 +28,17 @@ class PCLClient
 		tcp::socket socket_;
 		unsigned int nr_points = 0;
 		pcl::visualization::CloudViewer viewer_;
-		
+		pcl::PointCloud<pcl::PointXYZRGB>::Ptr buf_;
+
 		void handle_connect(const boost::system::error_code& error);
 		void do_close() { socket_.close(); };
+		pcl::PointCloud<pcl::PointXYZRGB>::Ptr getRegSeg();
 
 	public:
 		PCLClient(boost::asio::io_service& io_service, tcp::resolver::iterator endpoint_iterator) : io_service_(io_service), socket_(io_service), viewer_("PCL") {
 			boost::asio::async_connect(socket_, endpoint_iterator, boost::bind(&PCLClient::handle_connect, this, boost::asio::placeholders::error));
+			pcl::PointCloud<pcl::PointXYZRGB>::Ptr buf(new pcl::PointCloud<pcl::PointXYZRGB>);
+			buf_ = buf;
 		}
 		~PCLClient() { close(); };
 		void close() { io_service_.post(boost::bind(&PCLClient::do_close, this)); };
